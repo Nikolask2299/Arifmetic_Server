b9fb52d598e713242a87c69dcde0fe5498ae577d
Nikolask2299 <nikolasff889@yandex.ru>
2024-02-06 02:21:49 +0400

    22

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,8 +5,13 @@ import "time"
 type Config struct {
 	server string `json:"server"`
 	port int 	`json:"port"`
-	timeout time.Duration `json:`
-	storagePath string
-	CountAgent int
+	timeout time.Duration `json:"timeout"`
+	storagePath string `json:"storage_path"`
+	CountAgent int `json:"count_server_agent"`
 }
 
+ func Mustload() Config {
+
+
+	
+ }
